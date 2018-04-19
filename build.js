dfc74537d8f39ca2c4f942394fc79ac563d20aae
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-19 15:35:36 +0200

    clean code

--- build.js
+++ build.js
@@ -1,25 +1,27 @@
-const fs = require('fs-extra')
+const fse = require('fs-extra')
 const path = require('path')
 const glob = require('glob')
 
-const config = require('../../config')
-const renderer = require('./renderer')
+const renderer = require('./src/scripts/renderer')
+const config = require('./config')
 const { src, public } = config.path
 
-const render = renderer(config, [`${src}/global/*.json`])
+const render = renderer(config, {
+    global: `${src}/global/*.json`
+})
 
-fs.emptyDirSync(public)
-fs.copy(`${src}/assets`, `${public}/assets`)
+fse.emptyDirSync(public)
+fse.copy(`${src}/assets`, `${public}/assets`)
 
 glob('**/*.hbs', { cwd: `${src}/views` }, (error, files) => {
     files.forEach(file => {
         const { dir, name } = path.parse(file)
         const destPath = path.join(public, dir)
-        fs
+        fse
             .mkdirs(destPath)
             .then(() => render(`${src}/views/${file}`))
             .then(body => render(`${src}/layout.hbs`, { body }))
-            .then(html => fs.writeFile(`${destPath}/${name}.html`, html))
+            .then(html => fse.writeFile(`${destPath}/${name}.html`, html))
             .catch(err => console.error(err))
     })
 })
