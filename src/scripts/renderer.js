95b2e1d9f55001db83e4505c63890446b4606b88
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-19 15:29:13 +0200

    uses lodash get to find registered function

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -1,20 +1,18 @@
 const path = require('path')
 const glob = require('glob')
-
 const hbs = require('handlebars')
+const { get } = require('lodash')
+
 const createLoader = require('./loader')
 const template = require('./template')
 
-const { get } = require('lodash')
-
 hbs.registerHelper('fn', (name, options) => {
-    let { root } = options.data
-
-    let func = root.local && root.local[name]
-    if (typeof func !== 'function') {
-        return ''
+    let { root } = options.data,
+        method = get(root, name)
+    if (typeof method === 'function') {
+        return method(root)
     }
-    return func(root) || ''
+    return ''
 })
 
 module.exports = (config, loaders) => {
@@ -26,8 +24,8 @@ module.exports = (config, loaders) => {
         let data = Object.assign(
             load(filepath),
             context,
-            info,
-            config
+            config,
+            info
         )
         return hbs.compile(template(filepath))(data)
     }
