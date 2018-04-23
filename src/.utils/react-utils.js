9cd03f7c802098f361e75bc14e09eaa29c9b5f1b
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-23 14:04:25 +0200

    transform object properties

---
+++ src/.utils/react-utils.js
@@ -0,0 +1,53 @@
+import { get, isPlainObject } from 'lodash'
+
+export function choose(path, cases) {
+    if (isPlainObject(cases)) {
+        return object => {
+            let value = get(object, path)
+            return cases[String(value)]
+        }
+    }
+    return object => {
+        let value = get(object, path)
+        return value && cases
+    }
+}
+
+export function bind(context, methods) {
+    methods.forEach(item => {
+        context[item] = context[item].bind(context)
+    })
+}
+
+export function omit(object, props) {
+    if (object == null) {
+        return null
+    }
+    if (props == null || !props.length) {
+        return object
+    }
+    let newObject = {}
+    Object.keys(object).forEach(prop => {
+        if (props.indexOf(prop) < 0) {
+            newObject[prop] = object[prop]
+        }
+    })
+    return newObject
+}
+
+export function transform(object, props) {
+    if (object == null) {
+        return null
+    }
+    if (props == null) {
+        return object
+    }
+    let newObject = {}
+    Object.keys(object).forEach(prop => {
+        if (props[prop] !== null) {
+            let newProp = props[prop] !== undefined ? props[prop] : prop
+            newObject[newProp] = object[prop]
+        }
+    })
+    return newObject
+}
