a9971e4b59760f185df5da69b10a21c5670b7785
Bartosz Lorek <bery.lorek@gmail.com>
2018-04-24 00:27:24 +0200

    generic image component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import { transform } from '../.utils/react-utils'
 
 class Image extends React.PureComponent {
     render() {
-        let props = transform(this.props, {
-            filename: 'src',
-            group: 'data-group'
-        })
-        return <img {...props} src={`./images/${props.src}`} />
+        return <img {...this.props} />
     }
 }
 
