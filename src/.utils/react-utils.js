5adcba838d74bc32fa1d7b995a5ae7093039dbdb
Bartosz Lorek <bery.lorek@gmail.com>
2018-05-02 17:02:01 +0200

    add RenderPropComponent metods and clean code

--- src/.utils/react-utils.js
+++ src/.utils/react-utils.js
@@ -14,25 +14,35 @@ export function choose(path, cases) {
 }
 
 export function bind(context, methods) {
-    methods.forEach(item => {
-        context[item] = context[item].bind(context)
-    })
+    let index = methods != null ? methods.length : 0
+
+    while (index) {
+        let name = methods[--index]
+        if (context[name] !== undefined) {
+            context[name] = context[name].bind(context)
+        }
+    }
 }
 
-export function omit(object, props) {
+export function omit(object, props = []) {
     if (object == null) {
         return null
     }
     if (props == null || !props.length) {
         return object
     }
-    let newObject = {}
-    Object.keys(object).forEach(prop => {
+    let index = -1
+    const iterable = Object.keys(object)
+    const length = iterable.length
+    const result = {}
+
+    while (++index < length) {
+        let prop = iterable[index]
         if (props.indexOf(prop) < 0) {
-            newObject[prop] = object[prop]
+            result[prop] = object[prop]
         }
-    })
-    return newObject
+    }
+    return result
 }
 
 export function transform(object, props) {
@@ -42,12 +52,47 @@ export function transform(object, props) {
     if (props == null) {
         return object
     }
-    let newObject = {}
-    Object.keys(object).forEach(prop => {
+    let index = -1
+    const iterable = Object.keys(object)
+    const length = iterable.length
+    const result = {}
+
+    while (++index < length) {
+        let prop = iterable[index]
         if (props[prop] !== null) {
             let newProp = props[prop] !== undefined ? props[prop] : prop
-            newObject[newProp] = object[prop]
+            result[newProp] = object[prop]
         }
-    })
-    return newObject
+    }
+    return result
 }
+
+export function createMemo(memo = {}) {
+    return (name, callback, force) => {
+        if (memo[name] === undefined || force) {
+            memo[name] = callback
+        }
+        return memo[name]
+    }
+}
+
+function baseRenderProp(prop, args) {
+    if (prop == null) {
+        return null
+    }
+    if (typeof prop !== 'function') {
+        let propName = String(
+            (prop.type && prop.type.name) || prop.type || prop
+        )
+        if (propName.length > 50) {
+            propName = propName.slice(0, 50) + '...'
+        }
+        throw new TypeError(
+            `Given prop (${propName}) is render by Render Prop Component and should be a Function.`
+        )
+    }
+    return prop.apply(null, args)
+}
+
+export const applyRenderProp = baseRenderProp
+export const callRenderProp = (prop, ...args) => baseRenderProp(prop, args)
